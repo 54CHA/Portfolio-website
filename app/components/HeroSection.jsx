fc9d030b6b1830719223d2676ef5cee573ec9178
Alexander Khvoshchev <shsha.khv@gmail.com>
2023-10-15 18:27:43 +0300

    first commit

---
+++ app/components/HeroSection.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HeroSection = () => {
+  return (
+    <section>
+        <h1 className="text-white"> hello </h1>
+    </section>
+  )
+}
+
+export default HeroSection
---
+++ app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import HeroSection from "./components/HeroSection";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col bg-[#191919] container mx-auto py-4 px-12">
+      <HeroSection/>
+    </main>
+  
+  );
+}
