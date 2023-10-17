0896acf0854b5b8fc110330f15b30b549ab0f03d
Alexander Khvoshchev <shsha.khv@gmail.com>
2023-10-17 05:38:42 +0300

    design

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -6,27 +6,31 @@ const HeroSection = () => {
     <section>
       <div className="grid grid-cols-1 lg:grid-cols-12">
         <div className="col-span-7 place-self-center text-center sm:text-left">
-          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold mb-6">
-            Hello, I'm Alexander
+          <h1 className=" mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold mb-6">
+            <div className="text-transparent bg-clip-text bg-gradient-to-r from-yellow-300 to-orange-500">
+              Hello, I'm {""}
+            </div>
+            Alexander
           </h1>
           <p className="text-[#cfcfcf] text-base sm:text-lg mb-6">
-            sdfsdafsadfsdafsdafsadfsadfasdfasdffffff
+            sdfsdafsadfsdafsdafsadfsadfasdfasdffffffdsafasdfsadfsafdsfsdfsdfsadfasdfsdaf
+            sdfsadfdsfsadf sadfsadfsadfasdf
           </p>
-          
-            <button className=" px-6 py-3 rounded-full w-full sm:w-fit bg-bg-[#191919] hover:bg-slate-200 text-[#191919] font-semibold bg-white mr-4">
-              Download CV
-            </button>
-            
 
-            <button className="px-6 py-3 rounded-full w-full sm:w-fit bg-transparent hover:bg-slate-800 text-white font-semibold border border-white mt-3">
+          <button className="  px-6 py-3 rounded-full w-full sm:w-fit  text-[#191919] font-semibold bg-gradient-to-tr from-yellow-300 to-orange-500 mr-4">
+            <span className="hover:bg-orange-700">Download CV</span>
+          </button>
+
+          <button className="px-1 py-1 rounded-full w-full sm:w-fit bg-transparent bg-gradient-to-tr from-yellow-300 to-orange-500 hover:bg-slate-800 text-white font-semibold mt-3">
+            <span className="block bg-[#191919] hover:bg-slate-800 rounded-full px-5 py-2">
               Contact Me
-            </button>
-          
+            </span>
+          </button>
         </div>
 
         <div className="col-span-5 place-self-center mt-4 lg:mt-0">
           <div className="w-[250] h-[250] lg:w-[400] lg:h-[400] ">
-            <div className="p-2 bg-gradient-to-tr from-amber-500 to bg-fuchsia-700 rounded-full">
+            <div className="p-2 bg-gradient-to-tr from-yellow-300 to bg-orange-500 rounded-full">
               <div className="p-2 bg-[#191919] rounded-full">
                 <Image
                   src="/Images/pfp.jpeg"
