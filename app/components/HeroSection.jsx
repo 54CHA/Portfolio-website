c33dc013ee2e859cf88769d4b3a7994673d11681
Alexander Khvoshchev <shsha.khv@gmail.com>
2023-10-17 03:35:36 +0300

    fixed them again

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -13,23 +13,16 @@ const HeroSection = () => {
             sdfsdafsadfsdafsdafsadfsadfasdfasdffffff
           </p>
           <div className="flex">
-            <div className="p-2 bg-gradient-to-tr from-amber-500 to bg-fuchsia-700 rounded-full mr-4">
-              <div className=" rounded-full bg-[#191919]">
-                <button className=" px-6 py-3 rounded-full bg-bg-[#191919] hover:bg-slate-700 text-white font-semibold">
-                  Download CV
-                </button>
-              </div>
-            </div>
-            <div className="p-2 bg-gradient-to-tr from-amber-500 to bg-fuchsia-700 rounded-full">
-              <div className=" rounded-full bg-[#191919]">
-                <button className="px-6 py-3 rounded-full bg-[#191919] hover:bg-slate-700 text-white font-semibold">
-                  Contact Me
-                </button>
-              </div>
-            </div>
+            <button className=" px-6 py-3 rounded-full w-full sm:w-fit bg-bg-[#191919] hover:bg-slate-200 text-[#191919] font-semibold bg-white mr-4">
+              Download CV
+            </button>
+
+            <button className="px-6 py-3 rounded-full w-full sm:w-fit bg-transparent hover:bg-slate-800 text-white font-semibold border border-white">
+              Contact Me
+            </button>
           </div>
         </div>
-        
+
         <div className="col-span-5 place-self-center mt-4 lg:mt-0">
           <div className="w-[250] h-[250] lg:w-[400] lg:h-[400] ">
             <div className="p-2 bg-gradient-to-tr from-amber-500 to bg-fuchsia-700 rounded-full">
